96951f7610d9ae7a70fd99dd32a77e50beb50cdf
HostPrompt <hello@hostprompt.com>
2025-05-31 07:57:14 +0800

    Fix import extension for loadEnv in Vercel ESM environment

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
-import './loadEnv';
-
+import './loadEnv.js';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
