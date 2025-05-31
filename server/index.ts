249a8bf9b66dcab696c3638934d082907ba91be1
HostPrompt <hello@hostprompt.com>
2025-05-31 08:12:01 +0800

    Fix: Change import from './loadEnv.js' to './loadEnv' for Vercel TypeScript resolution

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
-import './loadEnv.js';
+import './loadEnv';
+
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
